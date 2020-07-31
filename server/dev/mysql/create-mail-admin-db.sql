a62c570036e8e23640dccf0d2f1c403d8ff6fd45
Bernhard Halbartschlager <halbart.bernhard+git@gmail.com>
2020-07-31 21:39:56 +0200

    enable liquibase

--- server/dev/mysql/create-mail-admin-db.sql
+++ server/dev/mysql/create-mail-admin-db.sql
@@ -1,5 +1,5 @@
 
-create database if not exists mailadmin;
+create database mailadmin default character set = 'utf8mb4' default collate = 'utf8mb4_unicode_ci';
 
 CREATE USER 'mailadmin'@'%' IDENTIFIED BY 'mailadmin' ;
 GRANT ALL ON `mailadmin`.* TO 'mailadmin'@'%';
